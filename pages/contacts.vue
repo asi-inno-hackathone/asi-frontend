1651d058bd14941d1c989b4cbf67ae646737bb09
VanishMax <maxkors2014@gmail.com>
2019-08-25 12:56:09 +0300

    Add contacts page

---
+++ pages/contacts.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="section">
+    <div class="div-block-7 top">
+      Контакты
+      <br />
+    </div>
+    <div class="w-container">
+      <div class="div-block-7 tt">
+        АСИ
+        <br />
+      </div>
+      <div class="div-block-10">
+        <div class="text-block-8">Vk</div>
+        <div class="text-block-9"><a href="https://vk.com/asi_ru" target="_blank">Агентство стратегических инициатив</a></div>
+      </div>
+      <div class="div-block-10">
+        <div class="text-block-8">Fb</div>
+        <div class="text-block-9"><a href="https://www.facebook.com/asi.russia/" target="_blank">Агентство стратегических инициатив</a></div>
+      </div>
+      <div class="div-block-10">
+        <div class="text-block-8">Cайт</div>
+        <div class="text-block-9"><a href="https://asi.ru/projects/submit_a_project/" target="_blank">Агентство стратегических инициатив</a></div>
+      </div>
+      <div class="div-block-10">
+        <div class="text-block-8">Телефон</div>
+        <div class="text-block-9">+7 495 690-91-29</div>
+      </div>
+      <div class="div-block-10">
+        <div class="text-block-8">Факс</div>
+        <div class="text-block-9">+7 495 690-91-39</div>
+      </div>
+      <div class="div-block-7 tt">
+        Служба психологической помощи
+        <br />
+      </div>
+      <div class="div-block-10">
+        <div class="text-block-8">Сайт</div>
+        <div class="text-block-9"><a href="http://aarussia.ru/tatarstan/">Ссылка</a></div>
+      </div>
+      <div class="div-block-10">
+        <div class="text-block-8">Телефон</div>
+        <div class="text-block-9">+7 843 253-50-13</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    head: {
+      title: 'Контакты',
+    },
+  };
+</script>
